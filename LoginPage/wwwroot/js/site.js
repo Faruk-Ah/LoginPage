66d7a81502d52ecff5607ece29df3388dc6dd6c9
Faruk-Ahmed <Faruk-Ahmed@FARUK-IT-JASHOR>
2024-02-15 11:08:16 +0600

    Add project files.

---
+++ LoginPage/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "admin" && password == "user") {
+        alert("Login successful");
+        return false;
+    }
+    else {
+        alert("Login Failed");
+    }
+}
